9e8bc44a84139bc575ffe78724139e0d7d986211
Adam <acjarrell@gmail.com>
2015-03-24 23:12:08 -0400

    Pulled where clause out of view

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,9 @@
 class Card < ActiveRecord::Base
   belongs_to :board
   validates :title, :board, :state, presence: true
+
+  scope :ready, -> { where(state: :ready) }
+  scope :doing, -> { where(state: :doing) }
+  scope :done, -> { where(state: :done) }
+
 end
