35bd9d42dd10cb5aab0fff0dc02f121f5ad5a402
CJarrell <colby.jarrell@icloud.com>
2015-03-30 22:01:56 -0400

    removed filler string for card.description

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ Board.destroy_all
 board = Board.create! title: "Building Little by Little"
 
 board.cards = [
-  Card.new(title: "Generate rails app", state: :done),
-  Card.new(title: "Make whole app", state: :ready),
-  Card.new(title: "Adding cards to the board", state: :doing)
+  Card.new(title: "Generate rails app", state: :done, description: "Set up new dir in terminal and rails new in that b"),
+  Card.new(title: "Make whole app", state: :ready, description: "Create a whole new super-app called little by little"),
+  Card.new(title: "Adding cards to the board", state: :doing, description: "create partial for cards")
   ]
 
