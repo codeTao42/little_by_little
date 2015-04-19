e0087d7655a73f25d9653ef61620326fa94e4870
Adam <acjarrell@gmail.com>
2015-04-19 16:40:29 -0400

    Added new cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,16 @@
 class CardsController < ApplicationController
+
+  def create
+    card = Card.new(card_params)
+    if card.save
+      flash[:notice] = "Your new card is up"
+      redirect_to board_path(current_user)
+    end
+  end
+
+  def show
+    @card = boards.cards.find(params[:id])
+  end
   
   def ready
     card = Card.find(params[:id])
@@ -16,5 +28,11 @@ class CardsController < ApplicationController
     card = Card.find(params[:id])
     card.update_attribute(:state, 'done')
     redirect_to root_path
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :state, :description, :board_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   post 'ready/:id', to: 'cards#ready', as: 'ready'
   post 'doing/:id', to: 'cards#doing', as: 'doing'
   post 'done/:id', to: 'cards#done', as: 'done'
-  resources :boards
+  
+  resources :boards, :cards
 end
